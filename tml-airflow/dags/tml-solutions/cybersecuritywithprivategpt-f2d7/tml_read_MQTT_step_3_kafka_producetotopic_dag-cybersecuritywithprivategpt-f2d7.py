96ea1c5c744f2240285e1e582b0d82cda9fc2828
Defendx3 <Defendx3@email>
2024-11-19 16:06:06 +0000

    Committing raspberrypi

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-f2d7/tml_read_MQTT_step_3_kafka_producetotopic_dag-cybersecuritywithprivategpt-f2d7.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-f2d7/tml_read_MQTT_step_3_kafka_producetotopic_dag-cybersecuritywithprivategpt-f2d7.py
@@ -30,7 +30,7 @@ default_args = {
   'producerid' : 'iotsolution',  
   'topics' : 'cisco-network-mainstream', # *************** This is one of the topic you created in SYSTEM STEP 2
   'identifier' : 'TML solution',  
-  'mqtt_broker' : '611fdf2e25df4870a78c98af8ce3b6c5.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
+  'mqtt_broker' : 'dc0c2bce573048948476ce00848243d4.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
   'mqtt_port' : '8883', # <<<******** Enter MQTT port i.e. 1883, 8883    (for HiveMQ cluster)
   'mqtt_subscribe_topic' : 'tml/cybersecurity', # <<<******** enter name of MQTT to subscribe to i.e. tml/iot  
   'mqtt_enabletls': '1', # set 1=TLS, 0=no TLSS  
